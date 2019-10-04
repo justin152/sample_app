4a13ca5591180cf2fd70ca522a4cc4bd8d305c20
justin152 <justinr@mtcpro.com>
2019-10-04 15:04:50 -0400

    Replaced fog gem with aws gem

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ group :test do
 end
 
 group :production do
-  gem 'fog', '1.42'
+  gem 'carrierwave-aws'
   gem 'pg', '0.20.0'
 end
 
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,18 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region => ENV['S3_REGION']
+    config.storage    =  :aws                  # required
+    config.aws_bucket =  ENV['S3_BUCKET']      # required
+    config.aws_acl    =  :private
+
+    config.aws_credentials = {
+      access_key_id:      ENV['S3_ACCESS_KEY'],       # required
+      secret_access_key:  ENV['S3_SECRET_KEY'],     # required
+      region:             ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_provider = 'fog/aws'
+
+    config.aws_attributes = {
+                              'Cache-Control'=>"max-age=#{365.day.to_i}",
+                              'Expires'=>'Tue, 29 Dec 2019 23:23:23 GMT'
+                            }
   end
 end
